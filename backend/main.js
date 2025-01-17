e6c4484d579ceff6a4b1b9123214169e9addff37
Ernesto Alejandro <piti08delgado@gmail.com>
2025-01-17 16:58:42 -0300

    add:feature logica de login

--- backend/main.js
+++ backend/main.js
@@ -1,20 +1,24 @@
-import express from 'express'
-import adminRoutes from './routes/adminRoutes.js'
-import facturasRoutes from './routes/facturasRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import { authenticateToken} from './middleware/auth.js'
+import express from 'express';
+import cors from 'cors'; // Importa el paquete cors
+import adminRoutes from './routes/adminRoutes.js';
+import facturasRoutes from './routes/facturasRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import { authenticateToken } from './middleware/auth.js';
 
-const app = express()
-app.use(express.json())
+const app = express();
 
 
+app.use(cors()); 
+
+app.use(express.json());
 
 // Rutas
-app.use('/v0/api/admin', authenticateToken,adminRoutes)
-app.use('/v0/api', authenticateToken, facturasRoutes)
-// Rutas públicas
-app.use('/auth', authRoutes)
+app.use('/v0/api/admin', authenticateToken, adminRoutes);
+app.use('/v0/api', authenticateToken, facturasRoutes);
+
+// Rutas públicas (sin autenticación)
+app.use('/auth', authRoutes);
 
 app.listen(3000, () => {
-    console.log("server is running on port 3000")
-})
+    console.log("Server is running on port 3000");
+});
